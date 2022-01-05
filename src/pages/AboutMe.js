4ffd51b0247bc497fa3330e27deb36e3691b8d6c
LixuanLuo <lixuanluo@hotmail.com>
2022-01-04 23:52:55 -0500

    about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import { Container } from "react-bootstrap";
+import me from "../img/me.png";
 
 const AboutMe = () => {
-    return (
-        <Container>
-            <h1>About Me</h1>
-        </Container>
-    )
-}
+  return (
+    <Container className="AboutMe container">
+      {/* <h1>About Me</h1> */}
+      <section className="flex flex-center AboutMe__content">
+        <img src={me} alt="me" />
+        <h1>Hi, I'm Lixuan Luo</h1>
+      </section>
+    </Container>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
