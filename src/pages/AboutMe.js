a378f026ad39789b5ffff4e02fc8ae992250ebbf
LixuanLuo <lixuanluo@hotmail.com>
2022-01-05 00:08:43 -0500

    about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -5,10 +5,20 @@ import me from "../img/me.png";
 const AboutMe = () => {
   return (
     <Container className="AboutMe container">
-      {/* <h1>About Me</h1> */}
       <section className="flex flex-center AboutMe__content">
         <img src={me} alt="me" />
-        <h1>Hi, I'm Lixuan Luo</h1>
+        <h1 className="AboutMe__title">Hi, I'm Lixuan Luo</h1>
+        <p className="AboutMe__description">
+          I am a Front-End Web Developer and Designer based in Toronto, with a
+          passion for creating engaging, entertaining user experiences. I have
+          developed a strong understanding of JavaScript, HTML, CSS, ReactJS and
+          more!
+        </p>
+        <br />
+        <p className="AboutMe__description">
+          I'm excited to make the leap and continue refining my skills with the
+          right company.
+        </p>
       </section>
     </Container>
   );
